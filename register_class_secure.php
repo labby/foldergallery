3f61dbb60e13351d7f05eb7544e023cc966128d1
labby <rp@cms-lab.com>
2018-02-21 08:49:14 +0100

    modify register class.secure

--- register_class_secure.php
+++ register_class_secure.php
@@ -11,32 +11,31 @@
  * 
  */
 
-global $lepton_filemanager;
-if (!is_object($lepton_filemanager)) require_once( "../../framework/class.lepton.filemanager.php" );
+
 
 $files_to_register = array(
-	'/modules/foldergallery_jq/add.php',
-	'/modules/foldergallery/delete.php',
-	'/modules/foldergallery/modify.php',
-	'/modules/foldergallery/modify_cat.php',
-	'/modules/foldergallery/modify_cat_sort.php',
-	'/modules/foldergallery/modify_settings.php',
-	'/modules/foldergallery/modify_thumb.php',     
-	'/modules/foldergallery/save_cat.php',
-	'/modules/foldergallery/save_files.php',
-	'/modules/foldergallery/save_settings.php',
-	'/modules/foldergallery/sync.php',
-	'/modules/foldergallery/help.php',
-	'/modules/foldergallery/backend.functions.php',
-	'/modules/foldergallery/delete_cat.php',
-	'/modules/foldergallery/delete_img.php',
- 	'/modules/foldergallery/move_down.php',
-	'/modules/foldergallery/move_up.php',
-	'/modules/foldergallery/quick_img_sort.php',
-	'/modules/foldergallery/reorderCNC.php',
-	'/modules/foldergallery/reorderDND.php'
+	'add.php',
+	'delete.php',
+	'modify.php',
+	'modify_cat.php',
+	'modify_cat_sort.php',
+	'modify_settings.php',
+	'modify_thumb.php',     
+	'save_cat.php',
+	'save_files.php',
+	'save_settings.php',
+	'sync.php',
+	'help.php',
+	'backend.functions.php',
+	'delete_cat.php',
+	'delete_img.php',
+ 	'move_down.php',
+	'move_up.php',
+	'quick_img_sort.php',
+	'reorderCNC.php',
+	'reorderDND.php'
 );
 
-$lepton_filemanager->register( $files_to_register );
+LEPTON_secure::getInstance()->accessFiles( $files_to_register );
 
 ?>
